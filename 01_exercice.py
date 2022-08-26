efdf59cee209996ba63a0091f998108d60522681
diego madariaga <diegonicolasmadariaga@gmail.com>
2022-08-25 21:02:03 -0400

    feat: :fire: adds class 01 files

---
+++ 01_exercice.py
@@ -0,0 +1,4 @@
+two_digit_number = input("Enter a two-digit number: ")
+
+sum = int(two_digit_number[0]) + int(two_digit_number[1])
+print("The sum of the two digits is: " + str(sum))
---
+++ 01_variables_basicas.py
@@ -0,0 +1,21 @@
+# print hello world
+print("Hello World")
+#name = input("What is your name? ")
+
+
+# datetypes
+string = "Hello World"
+print(type(string))
+number = 1
+print(type(number))
+floatnume = 1.1
+print(type(floatnume))
+boolean = True
+print(type(boolean))
+
+# concatenation
+text = "Hello" + " " + "World"
+print(text)
+num_chars = len(text)
+respuesta = "La respuesta es: " + str(num_chars)
+print(respuesta)
